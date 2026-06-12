167b661ca7f25e1fa96571417cbaf83fb9d68028
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-06-12 18:33:41 +0530

    chat with multiple user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
-# from typing import List
+from typing import List
 
 
 app = FastAPI()
@@ -13,15 +13,42 @@ app.add_middleware(
     allow_headers = ["*"],
 )
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message:str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    #Accept and track the new connection
+    await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            print(data)
-            await websocket.send_text(f"Message recieved: {data}")
+            await manager.broadcast(f"Someone said: {data}")
+
     except WebSocketDisconnect:
-        print("Client disconnected gracefully.")
+        manager.disconnect(websocket)
+        await manager.broadcast("A user has left the chat.")
+    # await websocket.accept()
+    # try:
+    #     while True:
+    #         data = await websocket.receive_text()
+    #         print(data)
+    #         await websocket.send_text(f"Message recieved: {data}")
+    # except WebSocketDisconnect:
+    #     print("Client disconnected gracefully.")
         
